5c4bb4ecf8678182624a02b0f1e8fc7f764693b0
retrocodes12 <88876293+retrocodes12@users.noreply.github.com>
2026-05-15 09:11:12 -0700

    Delete scratch.js

--- scratch.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('c:/Users/sohil/NebulaStreams/index.js', 'utf8');
-const lines = content.split('\n');
-const before = lines.slice(0, 165);
-const after = lines.slice(2700);
-fs.writeFileSync('c:/Users/sohil/NebulaStreams/index.js', before.join('\n') + '\n// REPLACEME\n' + after.join('\n'));
